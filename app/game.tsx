91be39a512b63129dbe2852bdb8fe00ac80639e7
s3xmaxman <badside3919@gmail.com>
2024-10-07 14:24:08 +0900

    update game.tsx

--- app/game.tsx
+++ app/game.tsx
@@ -32,23 +32,150 @@ import Animated, {
 const ROWS = 6;
 
 const game = () => {
+  const [word, setWord] = useState(
+    words[Math.floor(Math.random() * words.length)]
+  );
   const colorScheme = useColorScheme();
   const backgroundColor = Colors[colorScheme ?? "light"].gameBg;
   const textColor = Colors[colorScheme ?? "light"].text;
   const grayColor = Colors[colorScheme ?? "light"].gray;
   const router = useRouter();
+  const wordLetters = word.split("");
 
   const [rows, setRows] = useState<string[][]>(
     new Array(ROWS).fill(new Array(5).fill(""))
   );
   const [curRow, setCurRow] = useState(0);
-  const [curCol, setCurCol] = useState(0);
+  const [curCol, _setCurCol] = useState(0);
 
   const [greenLetters, setGreenLetters] = useState<string[]>([]);
   const [yellowLetters, setYellowLetters] = useState<string[]>([]);
   const [grayLetters, setGrayLetters] = useState<string[]>([]);
 
-  const addKey = (key: string) => {};
+  const colStateRef = useRef(curCol);
+  const setCurCol = (col: number) => {
+    colStateRef.current = col;
+    _setCurCol(col);
+  };
+
+  /**
+   * キーボードから入力されたキーを処理する
+   * ENTERキーが押された場合には、checkWordを実行
+   * BACKSPACEキーが押された場合には、現在の列の前のセルを空にしてカーソルを戻す
+   * それ以外の場合は、現在の列にキーを入力してカーソルを進める
+   * @param key 入力されたキー
+   */
+  const addKey = (key: string) => {
+    // 現在の列の状態をログ出力
+    console.log("CURRENT: ", colStateRef.current);
+
+    // rowsのコピーを作成
+    const newRows = [...rows.map((row) => [...row])];
+
+    // ENTERキーが押された場合
+    if (key === "ENTER") {
+      checkWord();
+      return;
+    }
+
+    // BACKSPACEキーが押された場合
+    if (key === "BACKSPACE") {
+      // 先頭の列であれば、0番目のセルを空にする
+      if (colStateRef.current === 0) {
+        newRows[curRow][0] = "";
+        setRows(newRows);
+        return;
+      }
+
+      // それ以外の場合、現在の列の前のセルを空にしてカーソルを戻す
+      newRows[curRow][colStateRef.current - 1] = "";
+      setCurCol(colStateRef.current - 1);
+      setRows(newRows);
+      return;
+    }
+
+    // 最後の列に達している場合、キーを追加しない
+    if (colStateRef.current >= newRows[curRow].length) {
+      console.log("Reached the end of the line");
+      return;
+    }
+
+    // 通常のキーが押された場合、現在の列にキーを入力してカーソルを進める
+    console.log("🚀 ~ addKey ~ curCol", colStateRef.current);
+    newRows[curRow][colStateRef.current] = key;
+    setRows(newRows);
+    setCurCol(colStateRef.current + 1);
+  };
+
+  const checkWord = () => {
+    const currentWord = rows[curRow].join("");
+
+    if (currentWord.length < word.length) {
+      console.log("Not enough letters");
+      // TODO: キー入力のエラーメッセージ
+      return;
+    }
+
+    if (!allWords.includes(currentWord)) {
+      console.log("Not in word");
+      // TODO: キー入力のエラーメッセージ
+      return;
+    }
+
+    const newGreen: string[] = [];
+    const newYellow: string[] = [];
+    const newGray: string[] = [];
+
+    currentWord.split("").forEach((letter, index) => {
+      if (letter === wordLetters[index]) {
+        newGreen.push(letter);
+      } else if (word.includes(letter)) {
+        newYellow.push(letter);
+      } else {
+        newGray.push(letter);
+      }
+    });
+
+    setGreenLetters([...greenLetters, ...newGreen]);
+    setYellowLetters([...yellowLetters, ...newYellow]);
+    setGrayLetters([...grayLetters, ...newGray]);
+
+    setTimeout(() => {
+      if (currentWord === word) {
+        console.log("Correct!");
+      } else if (curRow + 1 >= rows.length) {
+        console.log("Game over!");
+      }
+    }, 1500);
+
+    setCurRow(curRow + 1);
+    setCurCol(0);
+  };
+
+  const getCellColor = (cell: string, rowIndex: number, cellIndex: number) => {
+    "worklet";
+    if (curRow > rowIndex) {
+      if (wordLetters[cellIndex] === cell) {
+        return Colors.light.green;
+      } else if (wordLetters.includes(cell)) {
+        return Colors.light.yellow;
+      } else {
+        return grayColor;
+      }
+    }
+    return "transparent";
+  };
+
+  const getBorderColor = (
+    cell: string,
+    rowIndex: number,
+    cellIndex: number
+  ) => {
+    if (curRow > rowIndex && cell !== "") {
+      return getCellColor(cell, rowIndex, cellIndex);
+    }
+    return Colors.light.gray;
+  };
 
   return (
     <View style={[styles.container, { backgroundColor }]}>
@@ -73,7 +200,7 @@ const game = () => {
           <View style={[styles.gameFieldRow]} key={`row-${rowIndex}`}>
             {row.map((cell, cellIndex) => (
               <View key={`cell-${rowIndex}-${cellIndex}`} style={styles.cell}>
-                <Text>{cell}</Text>
+                <Text style={styles.cellText}>{cell}</Text>
               </View>
             ))}
           </View>
